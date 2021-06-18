f0ffc3e4ac15c0849f45587bfb974e903eea4cdb
Adrian Marimon <marimon.dev@Adrians-MacBook-Pro.local>
2021-06-18 15:16:34 -0400

    added and edited books params

--- scraper.rb
+++ scraper.rb
@@ -2,31 +2,40 @@ require 'open-uri'
 require 'nokogiri'
 require 'csv'
 require 'json'
+require 'rubygems'
+require 'httparty'
 
 url = 'https://play.google.com/store/books/collection/cluster?clp=sgIqCiIKHHByb21vdGlvbl9lYm9va19mcmVlX3ByZXZpZXcQRBgBIgQIBQgs:S:ANO1ljKncLs&gsr=Ci2yAioKIgoccHJvbW90aW9uX2Vib29rX2ZyZWVfcHJldmlldxBEGAEiBAgFCCw%3D:S:ANO1ljLHWg0'
+url2 = 'https://play.google.com/store/books/collection/cluster?clp=8gMiCiAKGnByb21vdGlvbl9lYm9va190b3BzZWxsaW5nEEQYAQ%3D%3D:S:ANO1ljKR2RI&gsr=CiXyAyIKIAoacHJvbW90aW9uX2Vib29rX3RvcHNlbGxpbmcQRBgB:S:ANO1ljJtcVM'
 def scraper(url)
   doc = Nokogiri::HTML(URI.open(url))
   books = []
   count = 0
-
   doc.search(".b8cIId.ReQCgd a").each do |element|
     book_path = 'https://play.google.com' + element.attributes['href'].value
     book_doc = Nokogiri::HTML(URI.open(book_path), nil, Encoding::UTF_8.to_s)
 
     if !(book_doc.search(".sIskre .AHFaub span").text).empty?
-      name = book_doc.search(".sIskre .AHFaub span").text
+      title = book_doc.search('.sIskre .AHFaub span').text
       author = book_doc.search("span.ExjzWd a.hrTbp.R8zArc").text
       image = book_doc.search('div.hkhL9e img.T75of')[0].attributes['src'].value
       description = book_doc.search('div.W4P4ne meta')[0].attributes['content'].value.to_s
-      link = book_path
+      url = book_path
+      img_url = book_doc.search('div.oQ6oV div.hkhL9e img.T75of.h1kAub')[0].attributes['src'].value
+      price = book_doc.search('span.oocvOe button.LkLjZd.ScJHi.HPiPcc.IfEcue').text[1..5].to_f
 
-      books << { :name => name, :author => author, :image => image, :description => description, :link => link } if !name.nil?
+      books << { :title => title, :author => author, :image => image, :description => description, :url => url, :img_url => img_url, :price => price } if !title.nil?
       count += 1
+      break if count == 10
     end
   end
 
-  File.write('./books.json', JSON.dump(JSON.parse(books.to_json)))
+  # File.write('./books.json', JSON.dump(JSON.parse(books.to_json)))
 
+  books.each do |book|
+    p book
+  end
 end
 
-scraper(url)
+
+scraper(url2)
